1f8098f3e5dd29f89553e1540576623f5b79e21e
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-05-16 16:12:53 -0300

    req5 feito

--- src/App.js
+++ src/App.js
@@ -26,6 +26,34 @@ class App extends React.Component {
     }, this.validationFields);
   };
 
+  validationFields = () => {
+    const { cardName, cardDescription,
+      cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare } = this.state;
+    const maxAttribute = 90;
+    const maxAttributeSum = 210;
+    const isCardNameValid = cardName.length > 0;
+    const isCardDescriptionValid = cardDescription.length > 0;
+    const isCardImageValid = cardImage.length > 0;
+    const isCardRareValid = cardRare.length > 0;
+    const isCardAttr1Valid = Number(cardAttr1) >= 0 && Number(cardAttr1) <= maxAttribute;
+    const isCardAttr2Valid = Number(cardAttr2) >= 0 && Number(cardAttr2) <= maxAttribute;
+    const isCardAttr3Valid = Number(cardAttr3) >= 0 && Number(cardAttr3) <= maxAttribute;
+    const cardAttrSum = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    const isCardAttrSumValid = cardAttrSum <= maxAttributeSum;
+    const isFormValid = (isCardNameValid
+      && isCardDescriptionValid
+      && isCardImageValid
+      && isCardRareValid
+      && isCardAttr1Valid
+      && isCardAttr2Valid
+      && isCardAttr3Valid
+      && isCardAttrSumValid);
+    this.setState({
+      isSaveButtonDisabled: !(isFormValid),
+    });
+  };
+
   render() {
     const { cardName, cardDescription,
       cardAttr1, cardAttr2, cardAttr3,
